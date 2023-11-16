73341c8733398bd18998a08c1a7cc3adbc3e2f37
Dan Nguyen <62517806+DanNguyenN@users.noreply.github.com>
2023-11-16 17:19:58 -0600

    Able to work with the object

---
+++ Assets/Buttons.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPressing : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void debug()
+    {
+        Debug.Log("Button Highlighted");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
